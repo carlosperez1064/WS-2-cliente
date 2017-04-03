bc8a21500deb27e4bca741988868dce5b6469b9e
Diana CM <diana.cm08@gmail.com>
2017-04-02 21:21:30 -0600

    Add files via upload

---
+++ WS-2-cliente/AppCliente.py
@@ -0,0 +1,26 @@
+from flask import Flask, request, json
+from flask import render_template
+
+
+__author__ = 'Carlos Perez', 'Diana Camacho', 'Hillary Brenes'
+
+app = Flask(__name__)
+
+@app.route('/test')
+def test():
+    return "Hola"
+
+# ------------------------------------------------ MOSTRAR FORMULARIO --------------------------------------------------#
+@app.route('/formulario')
+def formulario():
+    print("Si")
+    return render_template('registro.html')
+
+# ------------------------------------------ MOSTRAR HTML PARA CONSULTAS -----------------------------------------------#
+@app.route('/consultas')
+def consultas():
+    return render_template('consultas.html')
+
+# ----------------------------------------------------- EJECUCIÓN ------------------------------------------------------#
+if __name__ == '__main__':
+    app.run(port=5000, host='127.0.0.1')
